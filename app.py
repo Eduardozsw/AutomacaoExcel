70542c1ca06c2d3ee08be23ee5b59b9d1e38d23a
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-06-02 11:09:18 -0300

    code funcionando

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-from openpyxl import load_workbook
+import io
 
-caminho_arquivo = "output/novos_dados.xlsx"
 
 
 st.set_page_config(page_title="Secretaria de Saúde", layout="wide")
@@ -10,140 +9,13 @@ st.set_page_config(page_title="Secretaria de Saúde", layout="wide")
 st.title("Regulação - Exames laboratoriais")
 st.write("Olá, seja bem-vindo(a) ao sistema de agendamento de exames!")
 
-nova_aba = pd.DataFrame(
-    {
-        "PACIENTE": [],
-        "Ácido úrico": [],
-        "Bilirrubina Total e Frações": [],
-        "Colesterol total": [],
-        "Colesterol HDL": [],
-        "Colesterol LDL": [],
-        "Creatinina": [],
-        "Glicose - glicemia": [],
-        "Insulina": [],
-        "TGO -AST - Transaminase Oxalacetica": [],
-        "TGP - ALT - Transaminase pirúvica": [],
-        "Triglicerídeos": [],
-        "Uréia": [],
-        "ABO - Fator RH": [],
-        "Hemograma completo": [],
-        "Plaquetas": [],
-        "TAP - Tempo de protrombina": [],
-        "TS - Tempo de sangramento - DUKE": [],
-        "ASLO - Antiestreptolisina": [],
-        "Fator reumatóide  - FR - Látex": [],
-        "PCR - Proteína C Reativa dosagem": [],
-        "FTA - ABS IGG p Sífilis": [],
-        "FTA - ABS IGM p Sífilis": [],
-        "VDRL quantitativo - VDRL com titulação": [],
-        "Hepatite A (HAV  IGM)": [],
-        "Hepatite A (HAV  IGG)": [],
-        "Hepatite B  - HBSAG": [],
-        "Hepatite B - Anti HBS": [],
-        "Hepatite B - Anti HBC IGM": [],
-        "Hepatite B - HBEAG": [],
-        "Hepatite C - Anti-HCV": [],
-        "HIV 1 + HIV 2 (ELISA)": [],
-        "Herpes simples IGG": [],
-        "Herpes Simples IGM": [],
-        "Folato - Ácido fólico - Vitamina B9": [],
-        "Imunoglobulina E - IGE": [],
-        "Beta-HCG - Gonadotrofina coriônica humana": [],
-        "Parasitológico de Fezes - Pesquisa de ovos e cistos de parasitas - EPF": [],
-        "Sumário de urina - EAS- Urina tipo 1": [],
-        "Homocisteína na urina": [],
-        "Urocultura antibiograma- Cultura de bactérias para identificação": [],
-        "Cultura para BAAR - Micobactéria - Tuberculose": [],
-        "Sódio - Na": [],
-        "Potássio - k": [],
-        "Cloreto - Cloro sérico": [],
-        "Fosfatase alcalina - FA - FAL": [],
-        "Proteínas Totais e Frações": [],
-        "Alfa-fetoproteína": [],
-        "Antitireoglobulina": [],
-        "antimicrossomas": [],
-        "T3 total (Triodotironina)": [],
-        "T4 total - Tiroxina": [],
-        "T4 livre - Tiroxina livre": [],
-        "TSH - Hormônio tireoestimulante": [],
-        "LH - Hormônio Luteinizante": [],
-        "FSH - Hormônio Folículo estimulante": [],
-        "GAMA GT - Gama glutamil transferase": [],
-        "Progesterona": [],
-        "PSA - Antígeno prostático específico": [],
-        "Mucoproteínnas - GPA - Alfa 1 glicoproteína ácida": [],
-        "Amilase": [],
-        "Lipase": [],
-        "Hba1C - Hemoglobina glicosilada - A1c": [],
-        "Vitamina B12 - Cobalamina": [],
-        "Vitamina D - 25 hidroxi D - 25OHD": [],
-        "CEA - Antígeno Carcinoembrionário": [],
-        "C3": [],
-        "C4": [],
-        "Cálcio": [],
-        "Cortisol": [],
-        "Capacidade de Fixação de Ferro": [],
-        "TOTG - Curva Glicêmica": [],
-        "CH50 - Complemento total": [],
-        "Citomegalovírus IGM": [],
-        "Citomegalovírus IGG": [],
-        "Coombs Direto -TAD - Teste Direto de Antiglobulina Humana": [],
-        "Coombs Indireto - TIA - Teste indireto de Antiglobulina Humana": [],
-        "Epstein Barr IGG": [],
-        "Epstein Barr IGM": [],
-        "Eletroforese de hemoglobina": [],
-        "Eletroforese de proteínas": [],
-        "Estrona - E1": [],
-        "Estradiol - Estrogênio": [],
-        "FAN - Fator antinúcleo - Anticorpos antinúcleo": [],
-        "Ferro sérico": [],
-        "Ferritina": [],
-        "Fibrinogênio": [],
-        "Fósforo": [],
-        "Desidrogenase lática - DHL - LDH": [],
-        "Magnésio": [],
-        "Microalbumina na urina": [],
-        "Prolactina": [],
-        "Paratormônio - PTH": [],
-        "Reticulócitos": [],
-        "Rubéola IGG": [],
-        "Rubéola IGM": [],
-        "Sangue oculto nas fezes": [],
-        "Testosterona Total": [],
-        "Testosterona livre": [],
-        "Toxoplasmose IGM": [],
-        "Toxoplasmose IGG": [],
-        "Transferrina": [],
-        "Tempo de tromboplastina p ativada - TTPA": [],
-        "VHS - Velocidade de Hemossedimentação": [],
-        "HTLV 1 + HTLV 2 (ELISA)": [],
-        "HTLV 1 + HTLV 2 (WESTERN BLOT)": [],
-        "Tempo de trombina": [],
-        "G6PD - Glicose6 fostato desidrogenase": [],
-        "Eritrograma (eritrócitos, hemoglobina, hematócritos)": [],
-        "Hemoglobina": [],
-        "Troponina": [],
-        "Hematócrito": [],
-        "PCR quantitativa - Proteína C Reativa quantitativa": [],
-        "CPK - Creatinofosfoquinase": [],
-        "CPK-MB": [],
-        "Ionograma": [],
-        "CPF": [],
-        "SUS": [],
-        "DATA DE NASCIMENTO": [],
-        "TELEFONE": [],
-        "POSTO": [],
-        "PROFISSIONAL SOLICITANTE": [],
-        "COLETA DOMICILIAR": [],
-    }
-)
 
 tab1, tab2 = st.tabs(["Marcação", "Conferir Marcação"])
 
 if "df" not in st.session_state:
     st.session_state.df = pd.DataFrame()
 
-#tela de pesquisa na planilha
+# tela de pesquisa na planilha
 with tab2:
     termo = st.text_input("Pesquisar")
     coluna_escolhida = st.selectbox(
@@ -153,6 +25,8 @@ with tab2:
     tabelaExcel = st.file_uploader("Escolha um arquivo XLSX", type="xlsx")
     if not tabelaExcel == None:
         dfs = pd.read_excel(tabelaExcel, sheet_name=None, header=0)
+        xls = pd.ExcelFile(tabelaExcel)
+        abas = xls.sheet_names
 
         for nome_planilha, df in dfs.items():
             if nome_planilha in ["Início", "Fim", "Mensal"]:
@@ -175,7 +49,7 @@ with tab2:
                 st.subheader(nome_planilha)
                 st.dataframe(df)
 
-#tela de marcação
+# tela de marcação
 with tab1:
     # Formulário de Marcação
     _, col1, col2, col3 = st.columns([0.5, 1, 1, 0.5])
@@ -194,25 +68,9 @@ with tab1:
         endereco = ""
         if cdomiciliar:
             endereco = st.text_input("Endereço")
-    with col3:
-        if tabelaExcel is not None:
-            criar = st.checkbox("Criar uma nova planilha")
-            if criar:
-                titulo = st.text_input("Qual o nome da nova planilha")
-                if titulo:  # Só tenta se tiver título
-                    try:
-                        with pd.ExcelWriter(tabelaExcel, engine="openpyxl", mode="a") as writer:
-                            nova_aba.to_excel(writer, sheet_name=titulo, index=False)
-            
-                            st.success(f"Aba '{titulo}' criada com sucesso!")
-                    except ValueError as e:
-                        st.error(f"Erro: provavelmente a aba '{titulo}' já existe.")
-                    except Exception as e:
-                        st.error(f"Erro inesperado: {e}")
 
-            
     st.markdown("---")  # linha divisória
-    
+
     options = st.multiselect(
         "Quais exames deseja marcar?",
         [
@@ -333,12 +191,14 @@ with tab1:
             "TROPONINA",
         ],
     )
+    if abas is not None:
+        planilha = st.pills("Qual planilha você deseja marcar", options=abas)
 
     st.write("Exames selecionados:", options)
 
     if st.button("Confirmar marcação"):
         new_row = {
-            "PACIENTE": paciente,
+            "Paciente": paciente,
             "Data Nascimento": data,
             "Nº TELEFONE": telefone,
             "PROFISSIONAL SOLICITANTE": psolicitante,
@@ -351,8 +211,23 @@ with tab1:
         for exame in options:
             new_row[exame] = 1
 
-        st.session_state.df = pd.concat(
-            [st.session_state.df, pd.DataFrame([new_row])], ignore_index=True
+        dfe = pd.read_excel(tabelaExcel, sheet_name=planilha)
+
+        dfe = pd.concat(
+            [dfe, pd.DataFrame([new_row])], ignore_index=True
         )
+
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='openpyxl') as writer:
+            # regrava todas as abas, incluindo a atualizada
+            for nome, aba_df in dfs.items():
+                if nome == planilha:
+                    aba_df = dfe  # usa a planilha atualizada
+                aba_df.to_excel(writer, sheet_name=nome, index=False)
+
         st.success("Exame marcado com sucesso")
-        st.dataframe(st.session_state.df)
+        st.download_button("Baixar nova planilha", data=output.getvalue(), file_name="planilha_atualizada.xlsx")
+
+
+        st.success("Exame marcado com sucesso")
+        st.dataframe(dfe)
